3564e98b4bedbfadcfc7d6e972e6ee0a4e8056e4
yshuai <stefan_ysh@foxmail.com>
2023-11-29 10:39:55 +0800

    chore:update user DTOs with Swagger API properties

--- src/user/dto/create-user.dto.ts
+++ src/user/dto/create-user.dto.ts
@@ -1,6 +1,12 @@
+import { ApiProperty } from "@nestjs/swagger";
+
 export class CreateUserDto {
-    username: string;
-    password: string;
-    age: number;
-    sex: string;
+  @ApiProperty({ description: "用户名", example: "admin" })
+  username: string;
+  @ApiProperty({ description: "密码", example: "123456" })
+  password: string;
+  @ApiProperty({ description: "年龄", example: 18 })
+  age: number;
+  @ApiProperty({ description: "性别", example: "男" })
+  sex: string;
 }
--- src/user/dto/update-user.dto.ts
+++ src/user/dto/update-user.dto.ts
@@ -1,4 +1,17 @@
-import { PartialType } from '@nestjs/mapped-types';
-import { CreateUserDto } from './create-user.dto';
+import { PartialType } from "@nestjs/mapped-types";
+import { ApiProperty } from "@nestjs/swagger";
 
-export class UpdateUserDto extends PartialType(CreateUserDto) {}
+import { CreateUserDto } from "./create-user.dto";
+
+export class UpdateUserDto extends PartialType(CreateUserDto) {
+  @ApiProperty({ description: "用户 ID，唯一标识", example: 1 })
+  id: number;
+  @ApiProperty({ description: "用户名", example: "admin" })
+  username: string;
+  @ApiProperty({ description: "用户密码", example: "password" })
+  password: string;
+  @ApiProperty({ description: "用户年龄", example: 18 })
+  age: number;
+  @ApiProperty({ description: "用户性别", example: "男" })
+  sex: string;
+}
